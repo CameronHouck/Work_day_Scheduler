a55546d9824d4171d25e72339214a13525b56199
Cameron Houck <cameronhouck1@gmail.com>
2021-05-13 01:09:48 -0400

    adding local storage save

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,10 @@ function liveTime() {
     $("#moment-time").html(moment().format("MMMM D YYYY hh:mm A"));
   }
   setInterval(liveTime, 1000);
+
+  function setLocalStorage(objKey, objValue) {
+    var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+    dailySchedule[objKey] = objValue;
+    localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
+  }
+  
