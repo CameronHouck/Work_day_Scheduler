a13bb99c2ed7e4d8a4e40ccfeada6b372fa81d2e
Cameron Houck <cameronhouck1@gmail.com>
2021-05-13 02:12:22 -0400

    adding table color logic according to time

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,31 @@ $(document).ready(function () {
     });
   });
 });
+
+$(".timeslot").each(function () {
+    var time = $(this).data("time"); 
+    var now = moment().hour(); 
+    if (time < now) {
+      $(this).parent().css({ backgroundColor: "#A9A9A9" }); 
+    } else if (time == now) {
+      $(this).parent().css({ backgroundColor: "#DC143C" }); 
+      $(this)
+        .siblings()
+        .next()
+        .append(
+          "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
+            time +
+            "' data-target='#editEvent' type='submit'>Add Event</button>"
+        );
+    } else if (time > now) {
+      $(this).parent().css({ backgroundColor: "#32CD32" });
+      $(this)
+        .siblings()
+        .next()
+        .append(
+          "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
+            time +
+            "' data-target='#editEvent' type='submit'>Add Event</button>"
+        );
+    }
+  });
