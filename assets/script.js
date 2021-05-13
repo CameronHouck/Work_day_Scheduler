c1826d614f56d04edfdf218acb848589dbfe2ee1
Cameron Houck <cameronhouck1@gmail.com>
2021-05-13 01:33:57 -0400

    adding insert text bootsrap prompt

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
 function liveTime() {
-    $("#moment-time").html(moment().format("MMMM D YYYY hh:mm A"));
-  }
-  setInterval(liveTime, 1000);
+  $("#moment-time").html(moment().format("MMMM D YYYY hh:mm A"));
+}
+setInterval(liveTime, 1000);
 
-  function setLocalStorage(objKey, objValue) {
-    var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
-    dailySchedule[objKey] = objValue;
-    localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
-  }
-  
+function setLocalStorage(objKey, objValue) {
+  var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+  dailySchedule[objKey] = objValue;
+  localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
+}
