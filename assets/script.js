435c4644840b7d06fec31c5052198200acd3a2cd
Cameron Houck <cameronhouck1@gmail.com>
2021-05-13 20:08:18 -0400

    adding popup modal and fixing table classes

--- assets/script.js
+++ assets/script.js
@@ -26,56 +26,55 @@ $(document).ready(function () {
 });
 
 $(".timearea").each(function () {
-    var time = $(this).data("time"); 
-    var now = moment().hour(); 
-    if (time < now) {
-      $(this).parent().css({ backgroundColor: "#A9A9A9" }); 
-    } else if (time == now) {
-      $(this).parent().css({ backgroundColor: "#DC143C" }); 
-      $(this)
-        .siblings()
-        .next()
-        .append(
-          "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
-            time +
-            "' data-target='#editEvent' type='submit'>Add Event</button>"
-        );
-    } else if (time > now) {
-      $(this).parent().css({ backgroundColor: "#32CD32" });
-      $(this)
-        .siblings()
-        .next()
-        .append(
-          "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
-            time +
-            "' data-target='#editEvent' type='submit'>Add Event</button>"
-        );
+  var time = $(this).data("time");
+  var now = moment().hour();
+  if (time < now) {
+    $(this).parent().css({ backgroundColor: "#A9A9A9" });
+  } else if (time == now) {
+    $(this).parent().css({ backgroundColor: "#DC143C" });
+    $(this)
+      .siblings()
+      .next()
+      .append(
+        "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
+          time +
+          "' data-target='#editEvent' type='submit'>Add Event</button>"
+      );
+  } else if (time > now) {
+    $(this).parent().css({ backgroundColor: "#32CD32" });
+    $(this)
+      .siblings()
+      .next()
+      .append(
+        "<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='" +
+          time +
+          "' data-target='#editEvent' type='submit'>Add Event</button>"
+      );
+  }
+});
+
+$(".editData").click(function (event) {
+  $("#eventDescription").val("");
+  var chosenTime = $(this).parent().siblings().prev().html();
+  $("#chosen-time").html(chosenTime);
+  var timeValue = $(this).parent().siblings().prev().data("time");
+  $("#chosen-time").attr("data-hourValue", timeValue);
+});
+
+$("#saveBtn").click(function () {
+  var eventInformation = $("#eventDescription").val();
+  var chosenTimeHTML = $(this)
+    .parent()
+    .siblings()
+    .prev()[1]
+    .querySelector("span");
+  var chosenTimeHTML = $(chosenTimeHTML);
+  var dataAttribute = chosenTimeHTML.attr("data-hourValue");
+  $(".eventInfo").each(function (i, object) {
+    var object = $(object);
+    if (object.attr("value") == dataAttribute) {
+      object.html(eventInformation);
+      setLocalStorage(dataAttribute, eventInformation);
     }
   });
-
-  $(".editData").click(function (event) {
-    $("#eventDescription").val("");
-    var chosenTime = $(this).parent().siblings().prev().html();
-    $("#chosen-time").html(chosenTime);
-    var timeValue = $(this).parent().siblings().prev().data("time");
-    $("#chosen-time").attr("data-hourValue", timeValue);
-  });
-
-$("#saveBtn").click(function () { 
-    var eventInformation = $("#eventDescription").val(); 
-    var chosenTimeHTML = $(this)
-      .parent()
-      .siblings()
-      .prev()[1]
-      .querySelector("span");
-    var chosenTimeHTML = $(chosenTimeHTML);
-    var dataAttribute = chosenTimeHTML.attr("data-hourValue");
-    $(".eventInfo").each(function (i, object) {
-      var object = $(object);
-      if (object.attr("value") == dataAttribute) {
-        object.html(eventInformation);
-        setLocalStorage(dataAttribute, eventInformation);
-      }
-    });
-  });
-
+});
